2788e424850179e9962efc8d627ed7d33ec2a430
Dimbio253 <abdourahman.alimohamed@gmail.com>
2023-02-10 15:30:56 +0100

    Nav burger/ pas fini

--- navigation.js
+++ navigation.js
@@ -9,3 +9,34 @@ document.querySelector('.nav').addEventListener('mouseenter', () => {
 document.querySelector('.megamenu').addEventListener('mouseleave', () => {
     document.querySelector('.megamenu').style.opacity = 0;
 });
+
+
+
+if (screen.width < 1000) {
+    const navItems = document.querySelectorAll('.nav li');
+    const megaCols = document.querySelectorAll('.megamenu--colunm');
+    console.log(megaCols.length);
+
+    for (let i = 0; i < navItems.length; i = i + 1) {
+        navItems[i].appendChild(megaCols[i]);
+    }
+
+}
+
+document.querySelector('.nav--burger').addEventListener('click', () => {
+    document.querySelector('.nav .nav--ul').style.display = "block";
+
+});
+
+
+
+let liens = document.querySelectorAll('.nav--lien');
+
+for (let i = 0; i < liens.length; i = i + 1) {
+    document.querySelectorAll('.nav--lien')[i].addEventListener('click', function () {
+
+        this.nextSibling.style.display = 'block';
+        this.style.display = 'none';
+
+    });
+}
