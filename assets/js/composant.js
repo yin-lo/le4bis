ceb8411ccd5d078836c0ffc4e3a6183bef866bcb
yin-lo <yinlo@hotmail.fr>
2023-02-10 15:29:38 +0100

    bidouilles

--- assets/js/composant.js
+++ assets/js/composant.js
@@ -1,10 +1,26 @@
-window.addEventListener('scroll', function(){
-	let Y=window.scrollY
-	if (Y==400) {
-		
-	};
-	console.log(Y);
-});
-
+// window.addEventListener('scroll', function(){
+// 	let Y=window.scrollY
+// 	if (Y>400) {
+// 		document.querySelector('.h2').classList.add('tracking-in-expand-fwd');
+// 	}
+// 	else{
+// 		document.querySelector('.h2').classList.remove('tracking-in-expand-fwd');
+// 	};
+// });
 
 // 400 1300 2000 2800
+
+let options = {
+	root: document,
+	rootMargin: '0px',
+	threshold: .1,
+};
+
+let target = document.querySelector('.h2');
+
+let observer = new IntersectionObserver(AddKeyframe, options);
+observer.observe(target);
+
+function AddKeyframe() {
+	document.querySelector('.h2').classList.add('tracking-in-expand-fwd');
+}
